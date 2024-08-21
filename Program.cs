81f5c342b225246832103c77eb9bec11f924cef8
jiyeon11 <leejiyeon415@gmail.com>
2024-08-21 09:11:14 +0900

    out 키워드 실습 2

--- Program.cs
+++ Program.cs
@@ -30,6 +30,12 @@ namespace CSAdv31
     }
     internal class Program
     {
+        static void NextPosition(int x, int y, int vx, int vy,
+        out int rx, out int ry)
+        {
+            rx = x + vx;
+            ry = y + vy;
+        }
 
         static void Main(string[] args)
         {
@@ -60,6 +66,14 @@ namespace CSAdv31
                 Console.WriteLine("숫자를 입력해주세요");
             }
 
-    }
+            int x = 0;
+            int y = 0;
+            int vx = 1;
+            int vy = 1;
+            Console.WriteLine("현재 좌표: (" + x + ", " + y + ")");
+            NextPosition(x, y, vx, vy, out x, out y);
+            Console.WriteLine("현재 좌표: (" + x + ", " + y + ")");
+
+        }
     }
 }
